96c3a57ef1613a700976f85897f35fa42b7f6ee2
marcos almendros <marcosdalmendros@gmail.com>
2020-04-16 18:36:58 -0300

    Updates

---
+++ precache-manifest.ffb006d83c47283d559c20545a459b31.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dbafbcdf6e382e961e82138f295312",
+    "url": "/Panaderia-Don-Pablo/index.html"
+  },
+  {
+    "revision": "196d36ae01688c4f8b95",
+    "url": "/Panaderia-Don-Pablo/static/css/main.25a05a86.chunk.css"
+  },
+  {
+    "revision": "969a8a213c07867abf9a",
+    "url": "/Panaderia-Don-Pablo/static/js/2.3c050bba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Panaderia-Don-Pablo/static/js/2.3c050bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196d36ae01688c4f8b95",
+    "url": "/Panaderia-Don-Pablo/static/js/main.396e51ab.chunk.js"
+  },
+  {
+    "revision": "eb353c0c2a7c5cfab719",
+    "url": "/Panaderia-Don-Pablo/static/js/runtime-main.644d3ec6.js"
+  },
+  {
+    "revision": "417b1ed51146909eb33718f5711a16c8",
+    "url": "/Panaderia-Don-Pablo/static/media/alfajor-chocolate.417b1ed5.jpg"
+  },
+  {
+    "revision": "74a9e40d805fd5e528c45ff1fcacdc64",
+    "url": "/Panaderia-Don-Pablo/static/media/alfajor-maicena.74a9e40d.jpg"
+  },
+  {
+    "revision": "2a37ba45d44fa29efb1c5b608365da82",
+    "url": "/Panaderia-Don-Pablo/static/media/alfajor-membrillo.2a37ba45.jpg"
+  },
+  {
+    "revision": "fe46dd1dcf9868e1ac7892d47720a020",
+    "url": "/Panaderia-Don-Pablo/static/media/brownie.fe46dd1d.jpg"
+  },
+  {
+    "revision": "498274967d9282bfeb956bf5ea17dcb5",
+    "url": "/Panaderia-Don-Pablo/static/media/cañoncitos.49827496.jpg"
+  },
+  {
+    "revision": "7361dfe80fe78d0e9e0cb61236f154e3",
+    "url": "/Panaderia-Don-Pablo/static/media/criollos-hojaldre.7361dfe8.jpg"
+  },
+  {
+    "revision": "0b97b284aacb8f7541dad6b619c15f86",
+    "url": "/Panaderia-Don-Pablo/static/media/cuernitos.0b97b284.jpg"
+  },
+  {
+    "revision": "5ade1754b37e5eb75e6992bc0950b537",
+    "url": "/Panaderia-Don-Pablo/static/media/factura-carasucia.5ade1754.jpg"
+  },
+  {
+    "revision": "8805204b312a080ce84d08416fb349c3",
+    "url": "/Panaderia-Don-Pablo/static/media/factura-crema.8805204b.jpg"
+  },
+  {
+    "revision": "31885a15dc4270c7264c50ebbfda5d82",
+    "url": "/Panaderia-Don-Pablo/static/media/factura-dulcedeleche.31885a15.jpeg"
+  },
+  {
+    "revision": "44a55a80343ce6d49df21c7d3c6bdf98",
+    "url": "/Panaderia-Don-Pablo/static/media/factura-membrillo.44a55a80.jpg"
+  },
+  {
+    "revision": "0c7f383d599837785be12e9081eec2a0",
+    "url": "/Panaderia-Don-Pablo/static/media/galleta-chips.0c7f383d.jpg"
+  },
+  {
+    "revision": "f4053c42c44fe6a45cb87f2d13d31742",
+    "url": "/Panaderia-Don-Pablo/static/media/galleta-miel.f4053c42.jpg"
+  },
+  {
+    "revision": "e5bf72dbe971f1a1c227bc05a4c73352",
+    "url": "/Panaderia-Don-Pablo/static/media/logotipo.e5bf72db.svg"
+  },
+  {
+    "revision": "f7a85486556ba3ff81f90d3d3de44807",
+    "url": "/Panaderia-Don-Pablo/static/media/medialuna-dulcedeleche.f7a85486.jpg"
+  },
+  {
+    "revision": "e8423b654c6d987445274777fa1bdcb4",
+    "url": "/Panaderia-Don-Pablo/static/media/medialunas.e8423b65.jpg"
+  },
+  {
+    "revision": "30b05f517d94178104fb91516d302939",
+    "url": "/Panaderia-Don-Pablo/static/media/muffin.30b05f51.jpg"
+  },
+  {
+    "revision": "f7f876a32d142a4e95ab278eea5779f7",
+    "url": "/Panaderia-Don-Pablo/static/media/palmeritas.f7f876a3.jpg"
+  },
+  {
+    "revision": "6c5646bce1975df31224c607eac02f27",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-casero.6c5646bc.jpg"
+  },
+  {
+    "revision": "0461fa035d30ebbc66b5132f26043560",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-cereales.0461fa03.jpg"
+  },
+  {
+    "revision": "91a376834e00e07aae07a07d4a8c0746",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-flautin.91a37683.jpg"
+  },
+  {
+    "revision": "d05bab9d9e06fb88446b833ad54e243c",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-fortificado.d05bab9d.jpg"
+  },
+  {
+    "revision": "4e5913e1dc2d3ab3cf79a4503b0892f1",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-frances.4e5913e1.jpg"
+  },
+  {
+    "revision": "2c03d9a651fa30168d7181fdad6ad836",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-hamburguesa.2c03d9a6.jpg"
+  },
+  {
+    "revision": "6b95e08ac05459304770450704264083",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-individual.6b95e08a.jpg"
+  },
+  {
+    "revision": "aaac79435d767ccb995cb1db687daaa3",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-lomo.aaac7943.jpg"
+  },
+  {
+    "revision": "a825bfe36e82fd29f5ca0d8cf8ff94bf",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-miñon.a825bfe3.jpg"
+  },
+  {
+    "revision": "e7e7da7798ed16805d7b1239a9759f69",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-salvado.e7e7da77.jpg"
+  },
+  {
+    "revision": "baae34c64c6a87c19bf07c67990e8187",
+    "url": "/Panaderia-Don-Pablo/static/media/pan-viena.baae34c6.jpg"
+  },
+  {
+    "revision": "c2764a114626c0b4cfb7e253bb4a94a6",
+    "url": "/Panaderia-Don-Pablo/static/media/promos.c2764a11.jpg"
+  },
+  {
+    "revision": "cde2a897aab623c7b44ac914c3350662",
+    "url": "/Panaderia-Don-Pablo/static/media/torta-brownie.cde2a897.jpg"
+  },
+  {
+    "revision": "82f02ccaae18f3dcaf5d9b97ab1caa44",
+    "url": "/Panaderia-Don-Pablo/static/media/torta-durazno.82f02cca.jpg"
+  },
+  {
+    "revision": "0c601ea50a6ddd86a94cc3f99a1b5b01",
+    "url": "/Panaderia-Don-Pablo/static/media/torta-frutilla.0c601ea5.jpg"
+  },
+  {
+    "revision": "792b4520ab72d20569e94a420a2299db",
+    "url": "/Panaderia-Don-Pablo/static/media/torta-ganache.792b4520.jpg"
+  },
+  {
+    "revision": "1deee8e3e91cf6b2619df762d96c4dad",
+    "url": "/Panaderia-Don-Pablo/static/media/torta-milhojas.1deee8e3.jpg"
+  },
+  {
+    "revision": "13a103f57b6d35ea904e6269bacec7f8",
+    "url": "/Panaderia-Don-Pablo/static/media/torta-rogel.13a103f5.jpg"
+  },
+  {
+    "revision": "c35704892868a3348e349631595ae897",
+    "url": "/Panaderia-Don-Pablo/static/media/torta-selvanegra.c3570489.jpeg"
+  }
+]);
